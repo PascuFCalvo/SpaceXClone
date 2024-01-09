09829944970c39a442df28de1c068a6f4e0ebff3
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-01-09 20:56:44 +0100

    feat: fixing landing page

--- src/components/headerComponents/Navbar.jsx
+++ src/components/headerComponents/Navbar.jsx
@@ -1,73 +1,69 @@
 import Image from "next/image";
 import logo from "../../resources/logo.png";
+import Button from "@/app/landingPage/landinPageComponents/button/button";
 
 export default function Navbar() {
   return (
     <nav
-      className=" fixed w-100 flex items-center justify-between flex-wrap bg-transparent
-    p-6"
+      className=" fixed w-100 flex items-center justify-center  flex-wrap 
+    p-6   "
     >
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6 ">
         <Image src={logo} alt="logo" width={200} />
       </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto ">
         <div className="text-sm lg:flex-grow">
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 hover:border-b-2 "
           >
             FALCON 9
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 hover:border-b-2 "
           >
             FALCON HEAVY
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 hover:border-b-2 "
           >
             DRAGON
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 hover:border-b-2 "
           >
             STARSHIP
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 hover:border-b-2 "
           >
             HUMAN SPACEFLIGHT
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 hover:border-b-2 "
           >
             RIDESHARE
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 hover:border-b-2 "
           >
             STARSHIELD
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-20"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-20 hover:border-b-2 "
           >
             STARLINK
           </a>
         </div>
-        <div>
-          <a
-            href="#"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-white hover:bg-white mt-4 lg:mt-0"
-          >
-            SHOP
-          </a>
+        <div className="fixed top-0 right-0 mr-6 mt-6">
+          <Button watch={"API"} />
         </div>
       </div>
     </nav>
